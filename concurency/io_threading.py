7804c96262dcb50dece0f17486a74f191c0da97f
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2021-06-12 09:26:07 +0100

    add counter

--- concurency/io_threading.py
+++ concurency/io_threading.py
@@ -8,19 +8,25 @@ import time
 thread_local = threading.local()
 
 def get_session():
+    '''
+    So each thread will create a single session the first time it calls get_session() and then will simply use that session on each subsequent call throughout its lifetime.
+    '''
     if not hasattr(thread_local, "session"):
         thread_local.session = requests.Session()
     return thread_local.session
 
-def download_site(url, session):
+def download_site(url):
     session = get_session()
     with session.get(url) as response:
+
         print(f"Read {len(response.content)} from {url}")
 
+
 def download_all_sites(sites):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
         executor.map(download_site, sites)
-        
+
+
 if __name__ == "__main__":
     sites = [
         "https://www.jython.org",
